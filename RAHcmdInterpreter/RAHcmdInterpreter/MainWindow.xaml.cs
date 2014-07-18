0737f571658c405016bf3240ca9ce6c1e3cd49fa
David Niquette <niquetted@wit.edu>
2014-07-18 16:39:24 -0400

    First version, Added input/output richtextbox and tab control. No Parsing yet but all input text is handled and sent directly to output

---
+++ RAHcmdInterpreter/RAHcmdInterpreter/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RAHcmdInterpreter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        RAHCore core;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            core = new RAHCore();
+            setupNewLine();
+        }
+
+        public void setupNewLine()
+        {
+            InputText.AppendText("RAH >> ");
+        }
+
+        String getInputLine()
+        {
+            TextRange tRange = new TextRange(InputText.Document.ContentEnd.GetLineStartPosition(0), InputText.Document.ContentEnd);
+            String command = tRange.Text;
+            return command;
+        }
+
+        void writeToOutput(String message)
+        {
+            OutputText.AppendText(message);
+        }
+
+        void InputText_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                String input = getInputLine().Substring(7);
+                String result = core.ParseInput(input);
+                writeToOutput(result);
+            }
+        }
+
+        void InputText_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                setupNewLine();
+            }
+        }
+    }
+}
---
+++ RAHcmdInterpreter/RAHcmdInterpreter/RAHCore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAHcmdInterpreter
+{
+    class RAHCore
+    {
+        public String ParseInput(string input)
+        {
+            //TODO Parse
+            //
+            return input;
+        }
+    }
+}
